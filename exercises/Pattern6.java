9dc9642618f0f2ab29ae2f632c71387951a8ff99
Irsath Kareem <56153409+Mikkareem@users.noreply.github.com>
2023-12-04 13:29:12 +0530

    Create Pattern6.java

---
+++ exercises/Pattern6.java
@@ -0,0 +1,66 @@
+class Pattern {
+    public void printPattern(int n) {
+        int total = 2*n-1;
+        int center = total / 2;
+        for(int i = 0; i < total; i++) {
+            for(int j = 0; j < total; j++) {
+                if(i <= center) {
+                    // Conditions also symmetry
+                    if(j >= center-i && j <= center+i) {
+                        System.out.print("* ");
+                    } else {
+                        System.out.print("  ");
+                    }
+                } else {
+                    // Conditions also symmetry
+                    if(j >= center-(total-i-1) && j <= center+(total-i-1)) {
+                        System.out.print("* ");
+                    } else {
+                        System.out.print("  ");
+                    }
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Pattern pattern = new Pattern();
+        pattern.printPattern(3);
+        System.out.println();
+        pattern.printPattern(4);
+        System.out.println();
+        pattern.printPattern(5);
+        System.out.println();
+    }
+}
+
+/*
+Input = 3
+    *     
+  * * *   
+* * * * * 
+  * * *   
+    *     
+
+Input = 4
+      *       
+    * * *     
+  * * * * *   
+* * * * * * * 
+  * * * * *   
+    * * *     
+      *       
+
+Input = 5
+        *         
+      * * *       
+    * * * * *     
+  * * * * * * *   
+* * * * * * * * * 
+  * * * * * * *   
+    * * * * *     
+      * * *       
+        *         
+
+* */
