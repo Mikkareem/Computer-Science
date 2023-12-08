0de080d4bccd02a0a98eea96198fd41e3544d041
Irsath Kareem <56153409+Mikkareem@users.noreply.github.com>
2023-12-08 18:40:52 +0530

    Update String_Permutations.java

--- exercises/String_Permutations.java
+++ exercises/String_Permutations.java
@@ -1,4 +1,9 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 class Test {
+    // Method: 1
     private void permute(List<List<String>> answer, List<String> ds, char[] str, boolean[] freq) {
         if(ds.size() == str.length) {
             answer.add(new ArrayList<>(ds));
@@ -15,6 +20,7 @@ class Test {
         }
     }
 
+    // Method: 2
     private void permute(List<List<String>> answer, List<String> ds, int n, int[] count) {
         if(ds.size() == n) {
             answer.add(new ArrayList<>(ds));
@@ -31,9 +37,34 @@ class Test {
         }
     }
 
+    // Method: 3(Space Optimized)
+    private void permute(List<List<String>> ans, int index, char[] chars) {
+        if(index >= chars.length) {
+            ans.add(List.of(Arrays.toString(chars)));
+            return;
+        }
+
+        for(int i = index; i < chars.length; i++) {
+            // If we found duplicate elements, ignore it
+            if(i != index && chars[i] == chars[index]) {
+                continue;
+            }
+
+            swap(chars, i, index);
+            permute(ans, index+1, chars);
+            swap(chars, i, index);
+        }
+    }
+
+    private void swap(char[] chars, int a, int b) {
+        char temp = chars[a];
+        chars[a] = chars[b];
+        chars[b] = temp;
+    }
+
     public static void main(String[] args) {
         Test test = new Test();
-        String str = "AAB";
+        String str = "AAA";
         List<List<String>> answer = new ArrayList<>();
 
         // METHOD: 1
@@ -41,13 +72,16 @@ class Test {
 //        answer.forEach(System.out::println);
 
         // METHOD: 2
-        int[] count = new int[26];
-        for (char c: str.toCharArray()) count[c - 'A']++;
-        test.permute(answer, new ArrayList<>(), str.length(), count);
+//        int[] count = new int[26];
+//        for (char c: str.toCharArray()) count[c - 'A']++;
+//        test.permute(answer, new ArrayList<>(), str.length(), count);
+//        answer.forEach(System.out::println);
+
+        // METHOD: 3 (Space Optimized)
+        test.permute(answer, 0, str.toCharArray());
         answer.forEach(System.out::println);
     }
 }
-
 
 /*
 
